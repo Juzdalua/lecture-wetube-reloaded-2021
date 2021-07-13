e4cd51336443412d9d254e698fba8ff97d4831dd
Jun <bearwcw@naver.com>
2021-07-13 17:38:55 +0900

    Create server from express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-console.log("hi");
+import express from "express";
+
+//create server
+const PORT = 4000
+const app = express();
+const handleListening = () => console.log(`Server listening on port https://localhost:${PORT}`)
+app.listen(PORT, handleListening);
+
