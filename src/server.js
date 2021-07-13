bd609c512a8b301d42b238ccc1f2d2d7e42ea331
Jun <bearwcw@naver.com>
2021-07-13 18:11:19 +0900

    Simple request, response

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import express from "express";
 
 //create server
-const PORT = 4000
+const PORT = 4000;
 const app = express();
-const handleListening = () => console.log(`Server listening on port https://localhost:${PORT}`)
+
+//application setting
+const handleHome = (req, res) => {
+    return res.end(); //reqeust 종료 1
+}
+const handleLogin = (req, res) => {
+    return res.send("Login here.") //reqeust 종료 2
+}
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+//express code 
+const handleListening = () => 
+    console.log(`Server listening on port https://localhost:${PORT}`)
 app.listen(PORT, handleListening);
 
