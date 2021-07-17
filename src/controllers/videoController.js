2a913362faeb754b812c282afe1fa0e5463c0308
Jun <bearwcw@naver.com>
2021-07-18 06:19:07 +0900

    Update template. MVPstyle & mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,23 @@
-export const trending = (req, res) => res.render("home", {pageTitle : "Home"});
+export const trending = (req, res) => {
+    const videos = [
+        {
+        title:"one",
+        id:1,
+        views:30
+        },
+        {
+        title:"two",
+        id:2,
+        views:100
+        },
+        {
+        title:"three",
+        id:3,
+        views:50
+        }
+    ];
+    return res.render("home", {pageTitle : "Home", videos});
+}
 // render(name of view, variable)
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
