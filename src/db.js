c1c62783fa849de7ee31f5aa5f99d0b3bb389b35
Jun <bearwcw@naver.com>
2021-07-21 00:43:45 +0900

    Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//mongoose event
+const db = mongoose.connection;
+db.on("error", (error) => console.log("❌ DB Error", error));
+db.once("open", () => console.log("✅ Connected to DB"));
