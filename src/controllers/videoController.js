4bc16eb36154434ff1e7a649d6fe69af040b6329
Jun <bearwcw@naver.com>
2021-07-26 17:13:21 +0900

    Add db save in mongo

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,7 +6,7 @@ export const home = async(req, res) => {
     try{
         const videos = await Video.find({});
         // db를 받을 때까지 await이 JS를 기다려줌
-        return res.render("home", {pageTitle : "Home", videos:[]});
+        return res.render("home", {pageTitle : "Home", videos});
     } catch(error){
         return res.render("server-error");
     }
@@ -29,6 +29,33 @@ export const postEdit = (req, res) =>{
 export const getUpload = (req, res) => {
   return res.render("upload", {pageTitle : "Upload Video"});  
 };
-export const postUpload = (req, res) => {    
-    return res.redirect("/");
+export const postUpload = async (req, res) => {    
+    const {title, hashtags, description} = req.body; 
+        
+    // const video = new Video({ 
+    //     title, // title: title
+    //     description,
+    //     createdAt: Date.now(),
+    //     hashtags:hashtags.split(",").map(word => `#${word}`),
+    //     // "String".split(",") => ,를 기점으로 배열에 String을 분리
+    //     // array.map(word => `#${word}`) => 각 배열 인자마다 앞에 #을 달아줌
+    //     meta:{
+    //         views:0,
+    //         rating:0,
+    //     }   
+    // }); 
+    // const dbVideo = await video.save();
+    
+    try{
+        await Video.create({
+            title, // title: title
+            description,            
+            hashtags:hashtags.split(",").map(word => `#${word}`),
+            // "String".split(",") => ,를 기점으로 배열에 String을 분리
+            // array.map(word => `#${word}`) => 각 배열 인자마다 앞에 #을 달아줌            
+        }); 
+        return res.redirect("/");
+    } catch(error){
+        return res.render("upload", {pageTitle : "Upload Video", errorMessage: error._message}); 
+    }
 };
