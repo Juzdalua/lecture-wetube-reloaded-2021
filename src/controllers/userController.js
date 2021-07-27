20cb0cb110831ee02e21017a4098b4fa5e70541e
Jun <bearwcw@naver.com>
2021-07-28 02:47:15 +0900

    Added login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle:"Join"});
 export const postJoin = async (req, res) => {   
@@ -20,13 +21,39 @@ export const postJoin = async (req, res) => {
     }
 
     //create user
-    await User.create({
-        name, username, email, password, location
-    });
-    return res.redirect('/login');
+    try{
+        await User.create({
+            name, username, email, password, location
+        });
+        return res.redirect('/login');        
+    } catch(error){
+        return res.status(400).render("/join", {pagetitle:"Join", errorMessage:error._message});
     };
+} 
+
+export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
+export const postLogin = async (req, res) => {
+    const { username, password} = req.body;
+    const user = await User.findOne({username});
+
+    //check if account exists
+    if(!user){
+        return res.status(400).render("login", {pageTitle:"Login", errorMessage:"Username does not exists"});
+    }
+    //check if password correct
+    const ok = await bcrypt.compare(password, user.password); 
+    //return true or false
+    if(!ok){
+        return res.status(400).render("login", {pageTitle:"Login", errorMessage:"Password is wrong."});
+    }
+    
+    //session에 user 저장하기
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See User");
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import usersRouter from "./routers/userRouter";
+import {localsMiddleware} from "./middlewares";
 
 //create server
 const app = express();
@@ -13,6 +15,14 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
 
+// session. 사이트로 들어오는 모두를 기억. router 전에 작성!
+app.use(session({
+    secret: "hi",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", usersRouter);
